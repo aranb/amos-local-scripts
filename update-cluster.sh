c3d73c439aed45514e5611e8ae543c32f22c7fda
Aran Bergman <aran.bergman@gmail.com>
2016-03-22 14:08:14 +0200

    scripts modified for emulab experiments

--- update-cluster.sh
+++ update-cluster.sh
@@ -2,6 +2,8 @@
 
 export HBASE_VER=hbase-0.98.15-SNAPSHOT
 export HBASE_TAR=${HBASE_VER}-bin.tar.gz
+export PDIR="/proj/End2end/loctx/tars"
+export TARGET_HOST="aranb@users.emulab.net"
 
 if [ "$#" -lt 1 ] 
 then
@@ -13,17 +15,17 @@ export target=$1
 
 case ${target} in
 ycsb)
-scp ~/git/YCSB/distribution/target/ycsb-0.1.4.tar.gz aran@master:~/inst
+scp ~/git/ycsb/distribution/target/ycsb-0.1.4.tar.gz ${TARGET_HOST}:${PDIR}
 ;;
 hbase)
-scp ~/git/hbase/hbase-assembly/target/${HBASE_TAR} aran@master:~/inst
+scp ~/git/hbase/hbase-assembly/target/${HBASE_TAR} ${TARGET_HOST}:${PDIR}
 ;;
 tso)
-scp ~/git/omid/tso-server/target/tso-server-bin.tar.gz aran@master:~/inst
+scp ~/git/omid/tso-server/target/tso-server-*-bin.tar.gz ${TARGET_HOST}:${PDIR}
 ;;
 all)
-~/bin/update-cluster.sh hbase
-~/bin/update-cluster.sh tso
-~/bin/update-cluster.sh ycsb
+./update-cluster.sh hbase
+./update-cluster.sh tso
+./update-cluster.sh ycsb
 ;;
 esac	
